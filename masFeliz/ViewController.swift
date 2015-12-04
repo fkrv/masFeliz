660d3ac16db2a5ee0bb4d010c3c0793b015dba86
Angel Agustín Martínez <angel.agustin.martinez@gmail.com>
2015-12-03 21:05:37 -0600

    Initial Commit

---
+++ masFeliz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  masFeliz
+//
+//  Created by Angel Agustín Martínez on 03/12/15.
+//  Copyright © 2015 Angel Agustín Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
